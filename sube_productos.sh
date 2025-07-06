ea97b68aa2d94ad264b8ab653bc7dc60814016bd
Tm6-Telmedia <99041229+Tm6-Telmedia@users.noreply.github.com>
2025-07-05 18:29:57 -0600

    Update sube_productos.sh

--- sube_productos.sh
+++ sube_productos.sh
@@ -1,7 +1,7 @@
-if scp -P 65002 $1 u663923825@148.135.132.87:/home/u663923825/domains/store.telmedia.com.mx/public_html/
+if scp -P 65002 /cygdrive/c/inetpub/Telmedia/scripts/$1 u663923825@148.135.132.87:/home/u663923825/domains/store.telmedia.com.mx/public_html/
 then
-scp -P 65002 $1 u663923825@148.135.132.87:/home/u663923825/domains/store4.telmedia.com.mx/public_html/
-scp $1 root@10.240.128.6:/var/www/store/
+scp -P 65002 /cygdrive/c/inetpub/Telmedia/scripts/$1 u663923825@148.135.132.87:/home/u663923825/domains/store4.telmedia.com.mx/public_html/
+scp /cygdrive/c/inetpub/Telmedia/scripts/$1 root@10.240.128.6:/var/www/store/
 echo Subida de CSV OK 1>&2
 else
 echo Subida de CSV falló 1>&2
