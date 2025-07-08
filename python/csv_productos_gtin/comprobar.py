2a6be7c7b4e89bdaec7acdf42645d9492834af21
Luis Morán <82007006+Thoxics@users.noreply.github.com>
2025-07-08 09:56:22 -0600

    actualizacion script

--- python/csv_productos_gtin/comprobar.py
+++ python/csv_productos_gtin/comprobar.py
@@ -1,7 +1,9 @@
 import csv
 
+sku_buscado = input("Introduce el SKU que deseas buscar: ")
+
 with open('productos_actualizados.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        if row[2] == '19146':
+        if row[2] == sku_buscado:
             print(row)
