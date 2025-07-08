ff3d3f60dc05da848edceaf69e541d2bf58a0436
Luis Morán <82007006+Thoxics@users.noreply.github.com>
2025-07-08 09:55:15 -0600

    gtin productos

--- python/csv_productos_gtin/comprobar.py
+++ python/csv_productos_gtin/comprobar.py
@@ -3,5 +3,5 @@ import csv
 with open('productos_actualizados.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        if row[2] == '18628':
+        if row[2] == '19146':
             print(row)
