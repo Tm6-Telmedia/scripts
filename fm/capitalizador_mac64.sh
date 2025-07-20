0949a905ee9bb0e2cf75e78ae8f4d3337a07637e
Tm6-Telmedia <99041229+Tm6-Telmedia@users.noreply.github.com>
2025-07-19 18:02:15 -0600

    Update capitalizador_mac64.sh

--- fm/capitalizador_mac64.sh
+++ fm/capitalizador_mac64.sh
@@ -5,5 +5,5 @@ Palabra=\"$(echo $1|base64 -d|tr '\r' '^')\"
 echo $Palabra
 ssh administrator@$Server "capitalizacion.bat $Palabra"
 sleep 1
-scp administrator@$Server:/inetpub/Telmedia/scripts/traductor/capitalizacion.txt ~/Documents/txt_Traduccion.txt
+scp administrator@$Server:/inetpub/Telmedia/scripts/traductor/capitalizador.txt ~/Documents/txt_Traduccion.txt
 sleep 1
